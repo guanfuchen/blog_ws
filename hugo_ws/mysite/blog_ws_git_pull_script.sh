b29f7adaa6656c4dcd7f3fc2fbd4b94484052ed1
guanfuchen <guanfuchen@zju.edu.cn>
2018-06-05 14:14:18 +0800

    github blog workspace for hugo init

---
+++ hugo_ws/mysite/blog_ws_git_pull_script.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+blog_ws_root_path=~/GitHub/Quick/blog_ws
+
+echo ${blog_ws_root_path}
+
+# markdown_blog_ws for md
+if [ ! -d ${blog_ws_root_path}/hugo_ws/mysite/content/post/markdown_blog_ws ]; then
+    echo ${blog_ws_root_path}/hugo_ws/mysite/content/post/markdown_blog_ws
+    cd ${blog_ws_root_path}/hugo_ws/mysite/content/post
+    git clone git@gitee.com:chenguanfu/markdown_blog_ws.git markdown_blog_ws
+else
+    echo ${blog_ws_root_path}/hugo_ws/mysite/content/post/markdown_blog_ws
+    cd ${blog_ws_root_path}/hugo_ws/mysite/content/post/markdown_blog_ws
+    git pull
+fi
+
+# blog workspace for github
+if [ ! -d ${blog_ws_root_path}/hugo_ws/mysite/public ]; then
+    echo ${blog_ws_root_path}/hugo_ws/mysite/public
+    cd ${blog_ws_root_path}/hugo_ws/mysite
+    git clone git@gitee.com:chenguanfu/blog_ws_public.git public
+else
+    echo ${blog_ws_root_path}/hugo_ws/mysite/public
+    cd ${blog_ws_root_path}/hugo_ws/mysite/public
+    git pull
+fi
+
+# blog workspace for github
+if [ ! -d ${blog_ws_root_path}/hugo_ws/mysite/guanfuchen.github.io ]; then
+    echo ${blog_ws_root_path}/hugo_ws/mysite/guanfuchen.github.io
+    cd ${blog_ws_root_path}/hugo_ws/mysite
+    git clone git@github.com:guanfuchen/guanfuchen.github.io.git guanfuchen.github.io
+else
+    echo ${blog_ws_root_path}/hugo_ws/mysite/guanfuchen.github.io
+    cd ${blog_ws_root_path}/hugo_ws/mysite/guanfuchen.github.io
+    git pull
+fi
---
+++ hugo_ws/mysite/blog_ws_git_push_script.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+blog_ws_root_path=~/GitHub/Quick/blog_ws
+
+echo ${blog_ws_root_path}
+
+cd ${blog_ws_root_path}/hugo_ws/mysite/content/post/markdown_blog_ws
+git status -sb .
+git add . && git commit -m "update" && git push
+
+cd ${blog_ws_root_path}/hugo_ws/mysite
+hugo --config=config.toml
+
+cd ${blog_ws_root_path}/hugo_ws/mysite/public
+git status -sb .
+git add . && git commit -m "update" && git push
+
+cd ${blog_ws_root_path}/hugo_ws/mysite
+hugo --config=config-github.toml
+
+cd ${blog_ws_root_path}/hugo_ws/mysite/guanfuchen.github.io
+git status -sb .
+git add . && git commit -m "update" && git push
+
+#cd ${blog_ws_root_path}
+#git status -sb .
+#git add . && git commit -m "update" && git push
